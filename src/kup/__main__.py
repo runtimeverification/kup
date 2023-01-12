5567b296350df3ebef634f806b167edc19666e75
Sam Balco <goodlyrottenapple@gmail.com>
2023-01-12 14:07:15 +0000

    touch netrc file if it does not exist

--- src/kup/__main__.py
+++ src/kup/__main__.py
@@ -654,7 +654,8 @@ def add_new_package(
                 if not netrc_file:
                     netrc_file = os.path.join(config_path, 'netrc')
                     set_netrc_file(netrc_file)
-                    with open(netrc_file, 'a'):
+                if not os.path.exists(netrc_file):
+                    with open(netrc_file, 'a+'):
                         os.utime(netrc_file, None)
                 netrc = Netrc(netrc_file)
                 s_stripped = s.replace('https://', '').replace('http://', '').replace('/', '').strip()
