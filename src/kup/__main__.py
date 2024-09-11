f219408a7fa98c30b643b5a99540ada82025bdb7
Sam Balco <goodlyrottenapple@gmail.com>
2024-09-11 13:40:47 +0100

    Add kup update as alias for kup install

--- src/kup/__main__.py
+++ src/kup/__main__.py
@@ -888,6 +888,11 @@ def main() -> None:
     )
     install.add_argument('-h', '--help', action=_HelpInstallAction)
 
+    update = subparser.add_parser(
+        'update', help='update the stated package (alias of install)', add_help=False, parents=[verbose_arg, shared_args]
+    )
+    update.add_argument('-h', '--help', action=_HelpInstallAction)
+
     uninstall = subparser.add_parser(
         'uninstall', help="remove the given package from the user's PATH", parents=[verbose_arg]
     )
@@ -966,7 +971,7 @@ def main() -> None:
         if args.command == 'list':
             list_package(package_name.base, args.inputs, args.status, args.version)
 
-        elif args.command == 'install':
+        elif args.command == 'install' or args.command == 'update':
             install_package(package_name, args.version, args.override)
         elif args.command == 'uninstall':
             uninstall_package(package_name.base)
