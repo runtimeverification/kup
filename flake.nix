5572518d9be747fe3b68801090b57f6322e0afe9
Julian Kuners <julian.kuners@gmail.com>
2025-07-03 18:29:10 +0200

    Update nix flake inputs structure and add `rv-nix-tools` to a new dependency update workflow (#134)

--- flake.nix
+++ flake.nix
@@ -1,21 +1,39 @@
 {
   description = "kup";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    rv-nix-tools.url = "github:runtimeverification/rv-nix-tools/854d4f05ea78547d46e807b414faad64cea10ae4";
+    nixpkgs.follows = "rv-nix-tools/nixpkgs";
+  
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, rv-nix-tools, flake-utils, poetry2nix }:
     let
       allOverlays = [
-        poetry2nix.overlay
-        (final: prev: {
-          kup = prev.poetry2nix.mkPoetryApplication {
+        poetry2nix.overlays.default
+        (final: prev:
+        let
+          p2n = poetry2nix.lib.mkPoetry2Nix { pkgs = final; };
+        in {
+          kup = p2n.mkPoetryApplication {
             python = prev.python39;
             projectDir = ./.;
-            groups = [];
             # We remove `"dev"` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
             checkGroups = [];
+            overrides = p2n.defaultPoetryOverrides.extend
+              (self: super: {
+                tinynetrc = super.tinynetrc.overridePythonAttrs (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  }
+                );
+                git-url-parse = super.git-url-parse.overridePythonAttrs (
+                  old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  }
+                );
+              });
            };
         })
       ];
