3b4e473dc2f74c6f1ad64cd8cc35f892e5774267
Julian Kuners <julian.kuners@gmail.com>
2025-09-30 23:25:01 +0200

    Change kmir branch from `master` to `release` (#139)

--- src/kup/__main__.py
+++ src/kup/__main__.py
@@ -64,7 +64,7 @@ available_packages: list[GithubPackage] = [
     GithubPackage('runtimeverification', 'avm-semantics', PackageName('kavm')),
     GithubPackage('runtimeverification', 'evm-semantics', PackageName('kevm'), branch='release'),
     GithubPackage('runtimeverification', 'plutus-core-semantics', PackageName('kplutus')),
-    GithubPackage('runtimeverification', 'mir-semantics', PackageName('kmir')),
+    GithubPackage('runtimeverification', 'mir-semantics', PackageName('kmir'), branch='release'),
     GithubPackage('runtimeverification', 'kontrol', PackageName('kontrol'), branch='release'),
     GithubPackage('runtimeverification', 'kasmer-multiversx', PackageName('kmxwasm')),
     GithubPackage('runtimeverification', 'komet', PackageName('komet')),
