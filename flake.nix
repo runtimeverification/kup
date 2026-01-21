92efe54051752c6cfdac6c4377d53067bf743d9f
Andrei <16517508+anvacaru@users.noreply.github.com>
2026-01-21 14:40:17 +0200

    Revert "[DO NOT MERGE] bump python in flake.nix to 3.11"
    This reverts commit 70aad59c6590b8874104f3757c71d4259dcb7cb0.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           p2n = poetry2nix.lib.mkPoetry2Nix { pkgs = final; };
         in {
           kup = p2n.mkPoetryApplication {
-            python = prev.python311;
+            python = prev.python39;
             projectDir = ./.;
             # We remove `"dev"` from `checkGroups`, so that poetry2nix does not try to resolve dev dependencies.
             checkGroups = [];
